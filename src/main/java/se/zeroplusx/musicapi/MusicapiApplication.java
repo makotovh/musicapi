d53c30b5617b4c9f855d6768cd672dbc5393cd92
Makoto Hashimoto <makoto@makoto.blog.br>
2018-10-10 16:37:10 +0200

    add src

---
+++ src/main/java/se/zeroplusx/musicapi/MusicapiApplication.java
@@ -0,0 +1,12 @@
+package se.zeroplusx.musicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicapiApplication.class, args);
+	}
+}
